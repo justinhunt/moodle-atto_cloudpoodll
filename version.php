d44a6b96b78ecd40f6fefe8ad8c687ed6437b281
Tia <tia@techiasolutions.com>
2020-01-27 21:01:24 -0600

    Add history class, install code, and tests

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2019112100;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version = 2020012701;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires = 2015051100;        // Requires Moodle version 2.9 or greater
 $plugin->component = 'atto_cloudpoodll';  // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_STABLE;
