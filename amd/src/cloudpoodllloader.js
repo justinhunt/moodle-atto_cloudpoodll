61c3e12626e45bcc7fd5a5f27fe99a06e4a051bd
Tia <tia@techiasolutions.com>
2020-02-05 18:42:28 -0600

    Apply grunt updates

--- amd/src/cloudpoodllloader.js
+++ amd/src/cloudpoodllloader.js
@@ -1,19 +1,19 @@
 define(['jquery', 'core/log',
         'https://cdn.jsdelivr.net/gh/justinhunt/cloudpoodll@latest/amd/build/cloudpoodll.min.js',
         'core/str'],
-    function ($, log, CloudPoodll, str) {
+    function($, log, CloudPoodll, str) {
         return {
-            init: function (recorderclass, thecallback) {
+            init: function(recorderclass, thecallback) {
                 CloudPoodll.autoCreateRecorders(recorderclass);
                 CloudPoodll.theCallback = thecallback;
                 CloudPoodll.initEvents();
-                $("iframe").on("load", function () {
+                $("iframe").on("load", function() {
                     $(".assignsubmission_cloudpoodll_recording_cont").css('background-image', 'none');
                 });
 
                 // Load the history template for the history tab; this will call the history mustache template.
                 $('.atto_cloudpoodll_form li[data-content="history"]')
-                    .on("click", function () {
+                    .on("click", function() {
                         const loadingStr = str.get_string('loading', 'atto_cloudpoodll');
                         const loadingHtml = "<br /><div class=\"d-flex justify-content-center\">\n" +
                             "  <div class=\"spinner-border\" role=\"status\">\n" +
