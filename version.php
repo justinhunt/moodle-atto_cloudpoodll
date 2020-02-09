1eeda49afe0f55421b79c10bd6f2d3ce9d43d739
justinhunt <bitwalkerjapan@gmail.com>
2020-02-09 19:20:49 +0900

    version bump

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2020012711;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version = 2020020700;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires = 2015051100;        // Requires Moodle version 2.9 or greater
 $plugin->component = 'atto_cloudpoodll';  // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '1.0.15 (Build 2020012300)';
+$plugin->release = '1.0.16 (Build 2020020700)';
